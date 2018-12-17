4bfe30939c9dc86d863b5c54d9fb48d6013d123b
Vladyslav Popovych <mailuatc@gmail.com>
2018-12-17 14:24:26 +0200

    ~ Parsing and validation done

--- cpp_playground/core.hpp
+++ cpp_playground/core.hpp
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   core.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpopovyc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vpopovyc <vpopovyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 16:41:59 by vpopovyc          #+#    #+#             */
-/*   Updated: 2018/12/09 16:42:02 by vpopovyc         ###   ########.fr       */
+/*   Updated: 2018/12/17 14:18:03 by vpopovyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #pragma once
 
+// Tmp
+#include <iostream>
+#define GLM_ENABLE_EXPERIMENTAL
+
+// Include all GLM core / GLSL features & extensions
+// vec2, vec3, mat4, radians, perspective, translate, rotate
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include "glm/gtx/string_cast.hpp"
+
+// Parsing.cpp 
+// - STD
+#include <fstream>
+#include <stdexcept>
+#include <regex>
+#include <cstring>
+// - Custom
+bool inputIsGood(int ac, char *av[]);
+std::vector<glm::vec3> readFile(char *filePath);
--- cpp_playground/core/main.cpp
+++ cpp_playground/core/main.cpp
@@ -3,68 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpopovyc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vpopovyc <vpopovyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 16:40:21 by vpopovyc          #+#    #+#             */
-/*   Updated: 2018/12/09 16:40:22 by vpopovyc         ###   ########.fr       */
+/*   Updated: 2018/12/17 14:17:48 by vpopovyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <core.hpp>
-#include <fstream>
-#include <iostream>
-#include <stdexcept>
-#include <glm/glm.hpp>
-#include <regex>
-
-bool isReadable(char *filePath) {
-    std::ifstream fs(filePath);
-    return fs.is_open();
-}
-
-bool isInputOK(int ac, char *av[]) {
-    bool isInputOK = false;
-
-    if (ac == 2) {
-        isInputOK = isReadable(av[1]);
-    }
-    return isInputOK;
-}
-
-void readFile(char *filePath) {
-    std::ifstream file(filePath);
-    std::string line;
-    bool isEmpty = true;
-
-    const std::regex isVec3("\\s*(?:\\s*(\\d*)\\s*,\\s*(\\d*)\\s*,\\s*(\\d*)\\s*)\\s*"); 
-
-    while (std::getline(file, line)) {
-        isEmpty = false;
-        std::cout << "New line: " +  line + "\n";
-        std::smatch matches;
-        
-
-        while (std::regex_search(line, matches, isVec3)) {
-
-        int match_iter = 0;
-        for(auto submatch: matches) {
-            // First match is a full string
-            // All others is a values
-            std::cout << "s: " << submatch << std::endl;
-        }
-
-        }
-    }
-
-    if (isEmpty) {
-        throw std::invalid_argument("Get no data from read pass.");
-    }
-}
 
 int main(int ac, char *av[]) {
-    if (isInputOK(ac, av)) {
+    if (inputIsGood(ac, av)) {
         try {
-            readFile(av[1]);
+            auto controlPointsArray = readFile(av[1]);
+
+            for(auto &cp : controlPointsArray) {
+                std::cout << "Control point: " << glm::to_string(cp) << std::endl;
+            }
         } catch (std::exception &e) {
             std::cout << e.what() << std::endl;
         }
